15bc0dd22aab4879934d5c21be42d7b36a7a7557
dimasfirmanda27@gmail.com <dimasfirmanda27@gmail.com>
2024-08-30 22:55:55 +0700

    adding style and functionality: functionality: 1. add 2. operate 3. multiply
    style:
    1. add clear and delete button

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,92 @@
-const getScreen = document.getElementById('screen');
-getScreen.textContent = 0;
+const currentOperation = document.getElementById('current-operation');
+const memoryOperation = document.getElementById('memory-operation');
+const numberButton = document.querySelectorAll('[data-number]')
+const operatorButton = document.querySelectorAll('#operator');
+const evaluateButton = document.querySelector('#evaluate');
+const clearButton = document.querySelector('#clear');
 
-const getNumberButton = document.querySelectorAll('[data-number]')
-getNumberButton.forEach((button) => {
+let firstNum = '';
+let secondNum = '';
+let operation = null;
+
+function clearAll() {
+  firstNum = '';
+  secondNum = '';
+  operation = null;
+  currentOperation.textContent = 0;
+  memoryOperation.textContent = '';
+}
+
+function selectOperation(operator) {
+  if (firstNum !== '') {
+    operate();
+  }
+
+  operation = operator;
+  firstNum = secondNum;
+  secondNum = '';
+
+  memoryOperation.textContent = `${firstNum} ${operation}`
+}
+
+function displayValue(value) {
+  currentOperation.textContent = value;
+}  
+
+function insertNumber(number) {
+  secondNum += number;
+  displayValue(secondNum)
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function operate() {
+  let result;
+  let a = parseFloat(firstNum);
+  let b = parseFloat(secondNum);
+
+  switch(operation) {
+    case '+':
+      result = add(a,b)
+      break;
+    case 'x':
+      result = multiply(a,b);
+      break;
+    default:   
+      return;   
+  }
+
+  operation = '';
+  firstNum = '';
+  secondNum = result
+
+  displayValue(secondNum);
+  memoryOperation.textContent = '';
+}
+
+numberButton.forEach((button) => {
   button.addEventListener('click', () => {
-    getScreen.textContent = button.textContent;
+    insertNumber(button.textContent);
   })
 })
+
+operatorButton.forEach((button) => {
+  button.addEventListener('click', () => {
+    selectOperation(button.textContent);
+  })
+})
+
+evaluateButton.addEventListener('click', operate);
+clearButton.addEventListener('click', clearAll)
+
+
+
+
+
+
