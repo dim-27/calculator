d03df43ed52d12fba8d71bd3d7a1e0ee31f6627e
dimasfirmanda27@gmail.com <dimasfirmanda27@gmail.com>
2024-09-01 19:44:23 +0700

    adding functionalities: 1. keyboard input 2. division function 3. clear all and delete button

--- assets/script.js
+++ assets/script.js
@@ -4,29 +4,42 @@ const numberButton = document.querySelectorAll('[data-number]')
 const operatorButton = document.querySelectorAll('#operator');
 const evaluateButton = document.querySelector('#evaluate');
 const clearButton = document.querySelector('#clear');
+const deleteButton = document.querySelector('#delete');
 
-let firstNum = '';
-let secondNum = '';
+let memoryNum = '';
+let currentNum = '';
 let operation = null;
 
 function clearAll() {
-  firstNum = '';
-  secondNum = '';
+  memoryNum = '';
+  currentNum = '';
   operation = null;
   currentOperation.textContent = 0;
   memoryOperation.textContent = '';
 }
 
+function deleteNumber() {
+  if(currentOperation.textContent !== '') {
+    currentNum = currentNum.slice(0, -1);
+    displayValue(currentNum)
+  } 
+
+  if(currentOperation.textContent === '') {
+    currentOperation.textContent = 0;
+  }
+  
+}
+
 function selectOperation(operator) {
-  if (firstNum !== '') {
+  if (memoryNum !== '') {
     operate();
   }
 
   operation = operator;
-  firstNum = secondNum;
-  secondNum = '';
+  memoryNum = currentNum;
+  currentNum = '';
 
-  memoryOperation.textContent = `${firstNum} ${operation}`
+  memoryOperation.textContent = `${memoryNum} ${operation}`
 }
 
 function displayValue(value) {
@@ -34,39 +47,69 @@ function displayValue(value) {
 }  
 
 function insertNumber(number) {
-  secondNum += number;
-  displayValue(secondNum)
+  if (number === evaluateButton.textContent) return;
+  if (number === "." && currentNum.includes(".")) return;
+  if (currentNum.length > 10) return;
+  currentNum += number;
+  displayValue(currentNum)
 }
 
-function add(a, b) {
+function add(a,b) {
   return a + b;
+}
+
+function subtract(a,b) {
+  return a - b;
 }
 
 function multiply(a,b) {
   return a * b;
 }
 
+function divide(a,b) {
+  return a / b;
+}
+
 function operate() {
   let result;
-  let a = parseFloat(firstNum);
-  let b = parseFloat(secondNum);
+  let a = Number.parseFloat(memoryNum);
+  let b = Number.parseFloat(currentNum);
 
-  switch(operation) {
-    case '+':
-      result = add(a,b)
-      break;
-    case 'x':
-      result = multiply(a,b);
-      break;
-    default:   
-      return;   
+  
+  if(b === 0 && operation === "/") {
+    result = "ERROR!"
+  } else {
+    switch(operation) {
+      case '+':
+        result = add(a,b)
+        break;
+      case 'x':
+        result = multiply(a,b);
+        break;
+      case '/':
+        result = divide(a,b);
+        break;
+      case '-':
+        result = subtract(a,b);
+        break;
+      default:   
+        return;   
+    }
   }
 
-  operation = '';
-  firstNum = '';
-  secondNum = result
+  
 
-  displayValue(secondNum);
+  operation = '';
+  memoryNum = '';
+
+  currentNum = result.toString();
+  
+  if(currentNum.length > 10) {
+    currentNum =  Number.parseFloat(currentNum).toExponential(6)
+  }
+
+  
+  displayValue(currentNum);
   memoryOperation.textContent = '';
 }
 
@@ -82,8 +125,23 @@ operatorButton.forEach((button) => {
   })
 })
 
+window.addEventListener('keydown', function(event) {
+  if(event.defaultPrevented) return;
+
+  if((event.key >= 0 && event.key <= 9) || event.key === ".") insertNumber(event.key);
+  if(event.key === "Escape") clearAll();
+  if(event.key === "Backspace" || event.key === "Delete") deleteNumber();
+  if(event.key === "Enter" || event.key === "=") operate();
+  if(event.key === "+" || event.key === "-" || event.key === "/" || event.key === "x") selectOperation(event.key);
+
+
+  event.preventDefault();
+})
+
 evaluateButton.addEventListener('click', operate);
 clearButton.addEventListener('click', clearAll)
+deleteButton.addEventListener('click', deleteNumber);
+
 
 
 
